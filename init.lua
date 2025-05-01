e807cb1327a98f1b5f6935b528c362a6991f2513
rayvinvincent <raymondvincent.rv@gmail.com>
2025-05-01 10:06:09 +0100

    added live-preview plugin for markdown

--- init.lua
+++ init.lua
@@ -961,6 +961,13 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  {
+    'brianhuster/live-preview.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
